22ea5cf0fbd473fd034a93f13c66c7b2d6a58ef4
eric d smith <ericsmithdesigns@gmail.com>
2022-03-13 20:21:05 -0700

    Moved assets folder into src folder.

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -7,36 +7,36 @@ function Header() {
 
     return (
         <React.Fragment>
-            <nav class="navbar navbar-expand-sm navbar-dark bg-black sticky-top">
-                <a class="navbar-brand d-sm-none py-2" href="#"><img src="../img/logo.svg" width="40" heigh="40" /></a>
-                <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#billegalNavbar">
-                    <div class="hamburger" id="hamburger-2">
-                        <span class="line"></span>
-                        <span class="line"></span>
-                        <span class="line"></span>
+            <nav className="navbar navbar-expand-sm navbar-dark bg-black sticky-top">
+                <a className="navbar-brand d-sm-none py-2" href="#"><img src="../assets/images/logo.svg" width="40" heigh="40" /></a>
+                <button className="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#billegalNavbar">
+                    <div className="hamburger" id="hamburger-2">
+                        <span className="line"></span>
+                        <span className="line"></span>
+                        <span className="line"></span>
                     </div>
                 </button>
-                <div class="collapse navbar-collapse" id="billegalNavbar">
-                    <ul class="navbar-nav mx-auto text-white d-flex align-items-center">
-                        <li class="nav-item">
-                            <a class="nav-link" href="releases.html">RELEASES</a>
+                <div className="collapse navbar-collapse" id="billegalNavbar">
+                    <ul className="navbar-nav mx-auto text-white d-flex align-items-center">
+                        <li className="nav-item">
+                            <a className="nav-link" href="releases.html">RELEASES</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="tour.html">TOUR</a>
+                        <li className="nav-item">
+                            <a className="nav-link" href="tour.html">TOUR</a>
                         </li>
-                        <a class="px-3 py-2 d-none d-sm-block" href="#"><img src="img/logo.svg" alt="Billegal Beats" width="50"
+                        <a className="px-3 py-2 d-none d-sm-block" href="#"><img src="img/logo.svg" alt="Billegal Beats" width="50"
                             height="50" /></a>
-                        <li class="nav-item">
-                            <a class="nav-link d-none d-xl-block" href="#demosXL">DEMOS</a>
+                        <li className="nav-item">
+                            <a className="nav-link d-none d-xl-block" href="#demosXL">DEMOS</a>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link d-none d-block d-xl-none" href="#demos">DEMOS</a>
+                        <li className="nav-item">
+                            <a className="nav-link d-none d-block d-xl-none" href="#demos">DEMOS</a>
                         </li>
-                        <li class="nav-item mr-4 d-none d-sm-block">
-                            <a class="nav-link" href="shop.html">SHOP</a>
+                        <li className="nav-item mr-4 d-none d-sm-block">
+                            <a className="nav-link" href="shop.html">SHOP</a>
                         </li>
-                        <li class="nav-item d-block d-sm-none">
-                            <a class="nav-link" href="shop.html">SHOP</a>
+                        <li className="nav-item d-block d-sm-none">
+                            <a className="nav-link" href="shop.html">SHOP</a>
                         </li>
                     </ul>
                 </div>
